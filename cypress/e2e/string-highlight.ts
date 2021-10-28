6346dc3d6c187ee2eaa7b79724ac980baadb8654
Ákos Beke <akosbeke@gmail.com>
2021-10-28 12:50:45 +0200

    Wait for rendering the page before snapshot

--- cypress/e2e/string-highlight.ts
+++ cypress/e2e/string-highlight.ts
@@ -1,19 +1,19 @@
 describe('String highlights', () => {
   it('should match the visual snapshots for default string highlight', () => {
-    cy.visit('iframe.html?id=string-highlight--default')
-
-    cy.get('canvas').toMatchImageSnapshot()
+    cy.visit('iframe.html?id=string-highlight--default').then(() => {
+      cy.get('canvas').toMatchImageSnapshot()
+    })
   })
 
   it('should match the visual snapshots for custom color highlight', () => {
-    cy.visit('iframe.html?id=string-highlight--custom-color')
-
-    cy.get('canvas').toMatchImageSnapshot()
+    cy.visit('iframe.html?id=string-highlight--custom-color').then(() => {
+      cy.get('canvas').toMatchImageSnapshot()
+    })
   })
 
   it('should match the visual snapshots for multiple strings', () => {
-    cy.visit('iframe.html?id=string-highlight--multiple-strings')
-
-    cy.get('canvas').toMatchImageSnapshot()
+    cy.visit('iframe.html?id=string-highlight--multiple-strings').then(() => {
+      cy.get('canvas').toMatchImageSnapshot()
+    })
   })
 })
