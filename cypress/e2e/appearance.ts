6de866f18ad00bda867a43b3822bfe1bc3122168
Ákos Beke <akosbeke@gmail.com>
2021-10-28 13:06:55 +0200

    Debug CI

--- cypress/e2e/appearance.ts
+++ cypress/e2e/appearance.ts
@@ -1,30 +1,35 @@
 describe('Appearance', () => {
   it('should match the visual snapshots for the default variant', () => {
     cy.visit('iframe.html?id=appearance--default').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for the maple neck type', () => {
     cy.visit('iframe.html?id=appearance--neck-type-maple').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for the rosewood neck type', () => {
     cy.visit('iframe.html?id=appearance--neck-type-rosewood').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for dark mode', () => {
     cy.visit('iframe.html?id=appearance--dark-mode').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for custom dimensions', () => {
     cy.visit('iframe.html?id=appearance--custom-dimensions').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
@@ -32,12 +37,14 @@ describe('Appearance', () => {
   it('should match the visual snapshots for mobile', () => {
     cy.viewport(414, 896)
     cy.visit('iframe.html?id=appearance--mobile').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for custom background and padding', () => {
     cy.visit('iframe.html?id=appearance--background-and-padding').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
--- cypress/e2e/customization.ts
+++ cypress/e2e/customization.ts
@@ -1,30 +1,35 @@
 describe('Customization', () => {
   it('should match the visual snapshots for tunings or instruments', () => {
     cy.visit('iframe.html?id=customization--tuning-or-instruments').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for number of frets', () => {
     cy.visit('iframe.html?id=customization--number-of-frets').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for left handed fretboard', () => {
     cy.visit('iframe.html?id=customization--left-handed').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for hidden string labels', () => {
     cy.visit('iframe.html?id=customization--hide-string-labels').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
 
   it('should match the visual snapshots for hidden fret labels', () => {
     cy.visit('iframe.html?id=customization--hide-fret-labels').then(() => {
+      cy.wait(500)
       cy.get('canvas').toMatchImageSnapshot()
     })
   })
